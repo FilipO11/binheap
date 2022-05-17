34e9ccdb76bd6803b5aca13d4f64524acbc39720
filip <filip.opacic@outlook.com>
2022-05-17 18:10:47 +0200

    private vars + constructors + skeleton

---
+++ src/binheap.java
@@ -0,0 +1,32 @@
+public class binheap {
+    private String[] heap;
+    private int end = 0;
+    private int comp = 0;
+
+    public binheap() {
+        this.heap = new String[100];
+    }
+
+    public binheap(int size) {
+        this.heap = new String[size];
+    }
+
+    void insert(int key) {
+
+    }
+
+    void deleteMin() {
+
+    }
+    void printElements() {
+
+    }
+
+    void printMin() {
+
+    }
+
+    void printComparisons() {
+
+    }
+}

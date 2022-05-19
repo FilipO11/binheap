e49ca22c120838ff6286fe346ce769dc39754afb
filip <filip.opacic@outlook.com>
2022-05-19 19:37:47 +0200

    delete complete | 8/10

--- src/binheap.java
+++ src/binheap.java
@@ -33,21 +33,36 @@ public class binheap {
         System.out.printf("true: %d\n", heap[0]);
         swap(0, --end);
         heap[end] = 0; //full delete
+        if (end <= 1) return;
         bubbleDown();
     }
 
     private void bubbleDown() {
         int eltIdx = 0;
         int childIdx = 1;
+        int elt = heap[eltIdx];
+        int child = heap[childIdx];
+        comp++;
+        if (childIdx + 1 < end) {
+            if (child > heap[childIdx + 1]) childIdx++;
+        } else if (elt <= child) {
+            return;
+        }
+        swap(eltIdx, childIdx);
+        eltIdx = childIdx;
+        childIdx = eltIdx * 2 + 1;
+
         while (childIdx < end) {
-            int elt = heap[eltIdx];
-            int child = heap[childIdx];
+            elt = heap[eltIdx];
+            child = heap[childIdx];
             if (childIdx + 1 < end) {
                 comp++;
                 if (child > heap[childIdx + 1]) child = heap[++childIdx];
             }
-            comp++; //TODO: special case for first elt
-            if (elt <= child) return;
+            if (elt <= child) {
+                comp++;
+                return;
+            }
             swap(eltIdx, childIdx);
             eltIdx = childIdx;
             childIdx = eltIdx * 2 + 1;

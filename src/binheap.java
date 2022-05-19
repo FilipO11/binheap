ff9366bf7fa53b6fb8e5c25558ebddd5d8989332
filip <filip.opacic@outlook.com>
2022-05-19 11:17:22 +0200

    printElts, printMin, printCmps complete. removed extra contructor

--- src/binheap.java
+++ src/binheap.java
@@ -7,10 +7,6 @@ public class binheap {
         this.heap = new String[100];
     }
 
-    public binheap(int size) {
-        this.heap = new String[size];
-    }
-
     void insert(int key) {
 
     }
@@ -19,14 +15,28 @@ public class binheap {
 
     }
     void printElements() {
+        if (heap[0] == null) {
+            System.out.println("empty");
+            return;
+        }
 
+        StringBuilder sb = new StringBuilder(heap[0]);
+        int i = 1;
+
+        while (heap[i] != null) {
+            sb.append(", ");
+            sb.append(heap[i]);
+            i++;
+        }
+
+        System.out.println(sb);
     }
 
     void printMin() {
-
+        System.out.printf("MIN: %s", (heap[0] == null) ? "none" : heap[0]);
     }
 
     void printComparisons() {
-
+        System.out.printf("COMPARISONS: %d", comp);
     }
 }
